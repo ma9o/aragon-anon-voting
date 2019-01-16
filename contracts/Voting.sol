ec7b42f91c882f4eebc5734741dbbd57e32038dd
ma9o <46647883+ma9o@users.noreply.github.com>
2019-01-16 18:47:15 +0100

    Update Voting.sol

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -346,8 +346,7 @@ contract Voting is IForwarder, AragonApp {
     {
         Vote storage vote_ = votes[_voteId];
 
-        // This could re-enter, though we can assume the governance token is not malicious
-        uint256 voterStake = token.balanceOfAt(_voter, vote_.snapshotBlock);
+        uint256 voterStake = 1;
         VoterState state = vote_.voters[_voter];
 
         // If voter had previously voted, decrease count
